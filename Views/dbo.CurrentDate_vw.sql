7f05705b20694220b96a5a70acb286ea0d7e7301
Will Witte <willw@malibuboats.com>
2024-08-27 15:17:11 +0000

    added Tomorrow to the View

--- Views/dbo.CurrentDate_vw.sql
+++ Views/dbo.CurrentDate_vw.sql
@@ -4,20 +4,7 @@ SET ANSI_NULLS ON
 GO
 
 
-
-
---CREATE VIEW [dbo].[MBUU-STGDB01_SL_FL_App_job_mst]
---AS
---SELECT * FROM [SL_FL_App].[dbo].[job_mst];
---GO
-
---CREATE VIEW [dbo].[MB-CRM-DEV_MalibuDev_MSCRM_Contact]
---AS
---SELECT * FROM [MB-CRM-DEV].[MalibuDev_MSCRM].[dbo].[Contact];
---GO
-
-
 CREATE   VIEW [dbo].[CurrentDate_vw]
 AS
-SELECT CAST(GETDATE() AS date) AS CurrentDate,  CAST(GETDATE() AS TIME) AS CurrentTime, GETDATE() AS CurrentDateTime
+SELECT CAST(GETDATE() AS DATE) AS CurrentDate,  CAST(GETDATE() AS TIME) AS CurrentTime, GETDATE() AS CurrentDateTime ,CAST(GETDATE()+1 AS DATE) Tomorrow
 GO
